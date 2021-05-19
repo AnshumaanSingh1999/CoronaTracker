0eac0fa236e741761b5ae40963a9f1fb0fe56e3f
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2021-05-19 21:11:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function myFunction() {
 var btn = document.getElementById('Upload');
 btn.addEventListener('click', function() {
-  document.location.href = 'https://www.worldometers.info/coronavirus/';
+    document.getElementById("demo").innerHTML = "Currently Unavailable.";
 });
 }
